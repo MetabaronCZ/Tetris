b2b525ef9cd796a0554c5c26ac74406705607326
MetabaronCZ <33844039+MetabaronCZ@users.noreply.github.com>
2020-03-06 16:34:31 +0100

    Update GUI dimensions to keep aspect ratio with canvas View

--- src/scripts/engine/ui/components/App.tsx
+++ src/scripts/engine/ui/components/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { Provider } from 'react-redux';
 
-import { DEBUG_COUNTER } from 'engine/data/config';
+import { DEBUG_COUNTER, RENDER_WIDTH, RENDER_HEIGHT } from 'engine/data/config';
 
 import { GUIStore } from 'engine/ui/store';
 import Debug from 'engine/ui/components/Debug';
+
+const contentStyles: CSSProperties = {
+    maxWidth: RENDER_WIDTH + 'px',
+    width: 100 * (RENDER_WIDTH / RENDER_HEIGHT) + 'vh',
+    maxHeight: 100 * (RENDER_HEIGHT / RENDER_WIDTH) + 'vw'
+};
 
 interface Props {
     readonly store: GUIStore<any, any>;
@@ -12,8 +18,13 @@ interface Props {
 
 const App: React.SFC<Props> = ({ store, children }) => (
     <Provider store={store}>
+        <div className="Content">
+            <div className="Content-inner" style={contentStyles}>
+                {children}
+            </div>
+        </div>
+
         {DEBUG_COUNTER && <Debug />}
-        {children}
     </Provider>
 );
 
